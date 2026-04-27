c8722bc4b03fc6103193c7b3a227d36810592fb0
daaviddieeguez <daaviddieeguez@gmail.com>
2026-04-27 12:11:04 +0200

    Initial commit

---
+++ src/test/java/com/smart/gym/smartgymapi/SmartGymApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.gym.smartgymapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGymApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
