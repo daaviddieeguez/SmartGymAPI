2a8be1cfeacee6f8dacd7c77277fe69fd5cfff78
daaviddieeguez <daaviddieeguez@gmail.com>
2026-04-28 14:22:39 +0200

    feat: create GymService with constructor injection for member logic

---
+++ src/main/java/com/smart/gym/smartgym/service/GymService.java
@@ -0,0 +1,22 @@
+package com.smart.gym.smartgym.service;
+
+import com.smart.gym.smartgym.model.Member;
+import com.smart.gym.smartgym.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GymService {
+    private final MemberRepository memberRepository;
+
+    public void saveMember(Member member){
+        memberRepository.save(member);
+    }
+
+    public List<Member> getMembers() {
+        return memberRepository.findAll();
+    }
+}
