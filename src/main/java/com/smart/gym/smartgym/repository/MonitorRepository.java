2c150ecb346edb2e5ae3c1ab2d0c6633355826b2
daaviddieeguez <daaviddieeguez@gmail.com>
2026-04-29 09:54:18 +0200

    fix: Add tag Repository in MonitorRepository

--- src/main/java/com/smart/gym/smartgym/repository/MonitorRepository.java
+++ src/main/java/com/smart/gym/smartgym/repository/MonitorRepository.java
@@ -2,10 +2,12 @@ package com.smart.gym.smartgym.repository;
 
 import com.smart.gym.smartgym.model.Monitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MonitorRepository extends JpaRepository<Monitor, Long> {
     Optional<Monitor> findMonitorByDni(String dni);
 
